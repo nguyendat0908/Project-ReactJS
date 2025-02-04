4694cacec1e1139934dbfe0faecd09dbdaaa95ea
Nguyendat0908 <Nguyendat090803@gmail.com>
2025-02-04 15:25:16 +0700

    Lam quen voi State

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,14 +4,20 @@
 import React from "react";
 
 class MyComponent extends React.Component {
-    
+
+    state = {
+        name: 'Nguyen Hoang Dat',
+        address: 'Thai Binh',
+        age: 22
+    }
+
     // JSX
-    render(){
+    render() {
         return (
             <div>
-                My first component
+                My name is {this.state.name} and I'm from {this.state.address}
             </div>
-        );
+        )
     }
 }
 
