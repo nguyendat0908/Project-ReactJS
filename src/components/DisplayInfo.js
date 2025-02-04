480f3fadb893c22e3d87cb04a733c2879e213941
Nguyendat0908 <Nguyendat090803@gmail.com>
2025-02-04 23:18:01 +0700

    Sass voi react

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './DisplayInfo.scss';
 
 class DisplayInfo extends React.Component {
 
@@ -19,7 +20,7 @@ class DisplayInfo extends React.Component {
         console.log(listUser)
 
         return (
-            <div>
+            <div className="display-info-container">
 
                 <div>
                     <span onClick={() => { this.handleShowHide() }}>
