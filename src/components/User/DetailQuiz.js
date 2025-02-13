8d12be544b179b4a8292ef00c67acec3c53b697d
Nguyendat0908 <Nguyendat090803@gmail.com>
2025-02-13 15:53:12 +0700

    Process Data - Xu ly data phia FrontEnd

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,6 +1,7 @@
 import { use, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import  { getDataQuiz } from "../../services/ApiService";
+import { getDataQuiz } from "../../services/ApiService";
+import _ from 'lodash';
 
 const DetailQuiz = (props) => {
 
@@ -14,13 +15,32 @@ const DetailQuiz = (props) => {
 
     const fetchQuestions = async () => {
         let res = getDataQuiz(quizId);
-    }
+        if (res && res.EC === 0) {
+            let raw = res.DT;
+            // Lodash
+            let data = _.chain(raw).groupBy("id").map((value, key) => {
 
-    return (
-        <div className="detail-quiz-container container">
-            Detail Quiz
-        </div>
-    )
+                let answers = [];
+                let questionsDescription, image = null;
+                value.forEach((item, index) => {
+                    if (index === 0) {
+                        questionsDescription = item.description;
+                        image = item.image;
+                        
+                    }
+                    answers.push(item.answers)
+                })
+                return { questionId: key, answers, questionsDescription, image }
+            })
+                .value();
+        }
+
+        return (
+            <div className="detail-quiz-container container">
+                Detail Quiz
+            </div>
+        )
+    }
 }
 
 export default DetailQuiz;
