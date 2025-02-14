393f5266250facde731a8744444ff8b130df813a
Nguyendat0908 <Nguyendat090803@gmail.com>
2025-02-15 00:32:50 +0700

    Build data truoc khi submit API

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -76,6 +76,37 @@ const DetailQuiz = (props) => {
 
     }
 
+    const handleFinishQuiz = () => {
+
+        let payload = {
+            quizId: +quizId,
+            answers: []
+        };
+
+        let answers = [];
+        if (dataQuiz && dataQuiz.length > 0) {
+            dataQuiz.forEach(question => {
+                let questionId = question.questionId;
+                let userAnswerId = [];
+
+                // Todo: userAnswerId
+                question.answers.forEach(a => {
+                    if (a.isSelected === true) {
+                        userAnswerId.push(a.id);
+                    }
+                })
+
+                answers.push({
+                    questionId: +questionId,
+                    userAnswerId: userAnswerId
+                })
+            })
+
+            payload.answers = answers;
+            console.log("Final payload: ", payload)
+        }
+    }
+
     return (
         <div className="detail-quiz-container">
             <div className="left-content">
@@ -92,7 +123,7 @@ const DetailQuiz = (props) => {
                 <div className="footer">
                     <button className="btn btn-primary" onClick={() => handlePrev()}>Prev</button>
                     <button className="btn btn-secondary" onClick={() => handleNext()}>Next</button>
-                    <button className="btn btn-warning" onClick={() => handleNext()}>Finish</button>
+                    <button className="btn btn-warning" onClick={() => handleFinishQuiz()}>Finish</button>
                 </div>
             </div>
             <div className="right-content">
